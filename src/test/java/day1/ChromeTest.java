bc2b0f8c85d09ccb1a4c7dfe111e8f8f457c8044
Amit <amitpradhan.2011@gmail.com>
2026-04-26 09:46:14 +0530

    First commit

---
+++ src/test/java/day1/ChromeTest.java
@@ -0,0 +1,25 @@
+package day1;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ChromeTest {
+    public static void main(String[] args) {
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+
+//            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().
+//                    setExecutablePath(Paths.get("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"))
+//                    .setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/");
+            System.out.println(browser.browserType().name()+ "-->" + browser.version());
+        }
+    }
+}
---
+++ src/test/java/day1/HeadlessFalseTest.java
@@ -0,0 +1,18 @@
+package day1;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class HeadlessFalseTest {
+
+    public static void main(String[] args) {
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/");
+            System.out.println(browser.browserType().name() + "-->"+ browser.version());
+    }
+}}
